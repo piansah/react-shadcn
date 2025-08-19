905a8008d74876ba5d979f7f3f01ab815e81872d
Pian <alifalpian157@gmail.com>
2025-08-19 18:29:15 +0700

    dark mode - mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuLabel,
+} from "@/components/ui/dropdown-menu";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button
-        variant="outline"
-        size="lg"
-        onClick={() => alert("Button clicked!")}
-        className="rounded 2xl"
-      >
-        Click me
-      </Button>
-    </div>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
+      <div className="flex min-h-svh flex-col items-center justify-center p-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="default" size="lg" className="rounded 2xl">
+              Action
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Action</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => alert("Button Edit!")}>
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => alert("Button View!")}>
+              View
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem variant="destructive">Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </ThemeProvider>
   );
 }
 
