a25251479dfb8624720c6b9827a5771a4f5bb79f
Pian <alifalpian157@gmail.com>
2025-08-23 10:56:23 +0700

    card & profile card

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuLabel,
-} from "@/components/ui/dropdown-menu";
 import { ThemeProvider } from "./components/theme-provider";
 import { ModeToggle } from "./components/mode-toggle";
+import { Card, CardContent } from "./components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
@@ -17,25 +11,32 @@ function App() {
         <ModeToggle />
       </div>
       <div className="flex min-h-svh flex-col items-center justify-center p-4">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="default" size="lg" className="rounded 2xl">
-              Action
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>Action</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={() => alert("Button Edit!")}>
-              Edit
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => alert("Button View!")}>
-              View
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem variant="destructive">Delete</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Card className="p-6 shadow-lg rounded-2xl">
+          <CardContent className="flex flex-col items-center">
+            <Avatar className="h-24 w-24">
+              <AvatarImage
+                src="https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg"
+                alt="Avatar"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h1 className="text-lg font-semibold mt-4 text-center">Ghenn</h1>
+            <p className="text-sm text-muted-foreground text-center">
+              Web Developer
+            </p>
+            <div className="flex justify-center mt-4 gap-2">
+              <Button className="px-4 py-2 text-sm font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600">
+                Follow
+              </Button>
+              <Button
+                variant="outline"
+                className="px-4 py-2 text-sm font-semibold"
+              >
+                Profile
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </ThemeProvider>
   );
